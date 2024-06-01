af74c1a8e9411be03be82076c0a2f7311bf1160f
Roman <Roman.badiere@gmail.com>
2024-06-01 19:26:53 +0200

    hopefully target folder FINALLY GONE

--- target/debug/deps/dark_forest-2c600839ddb5af3a.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Romek\projects\dark_forest\target\debug\deps\dark_forest-2c600839ddb5af3a.exe: src\main.rs
-
-c:\Users\Romek\projects\dark_forest\target\debug\deps\dark_forest-2c600839ddb5af3a.d: src\main.rs
-
-src\main.rs:
--- target/debug/deps/dark_forest-ef0a04e5a07b3b43.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Romek\projects\dark_forest\target\debug\deps\libdark_forest-ef0a04e5a07b3b43.rmeta: src\main.rs
-
-C:\Users\Romek\projects\dark_forest\target\debug\deps\dark_forest-ef0a04e5a07b3b43.d: src\main.rs
-
-src\main.rs:
