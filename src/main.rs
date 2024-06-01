c9866e9192ad9f81de49b87b42e6f3295806eaaf
Roman <Roman.badiere@gmail.com>
2024-06-01 19:04:36 +0200

    menu not menuing + dev optimizations #7

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use bevy::prelude::*;
+use dark_forest::GamePlugin;
+
+#[derive(Component)]
+struct MyCameraMarker;
+
+fn camera_setup(mut commands: Commands) {
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(10.0, 12.0, 16.0)
+                .looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        MyCameraMarker,
+    ));
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_plugins(GamePlugin)
+    .add_systems(Startup, camera_setup)
+    .run();
 }
