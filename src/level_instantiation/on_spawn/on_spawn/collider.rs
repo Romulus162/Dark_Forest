c77b22beaf6b6bf0bc0548ebb4d0be5415aa8582
Roman Badiere <Roman.Badiere@gmail.com>
2024-07-22 06:58:59 +0200

    file cleaning

--- src/level_instantiation/on_spawn/on_spawn/collider.rs
+++ src/level_instantiation/on_spawn/on_spawn/collider.rs
@@ -1,54 +1,64 @@
-use crate::util::error;
-use crate::{movement::physics::CollisionLayer, GameSystemSet};
+use crate::{movement::physics::CollisionLayer, GamerState};
 use anyhow::Context;
-use bevy::prelude::*;
+use bevy::{prelude::*, transform::TransformSystem::TransformPropogate};
+use bevy_mod_sysfail::prelude::*;
 use bevy_xpbd_3d::prelude::{Collider as XpbdCollider, *};
 use oxidized_navigation::NavMeshAffector;
 use serde::{Deserialize, Serialize};
-use std::iter;
 
 #[derive(Debug, Clone, Eq, PartialEq, Component, Reflect, Serialize, Deserialize, Default)]
 #[reflect(Component, Serialize, Deserialize)]
 struct Collider;
 
 pub(super) fn plugin(app: &mut App) {
-    app.register_type::<Colllider>().add_systems(
+    app.register_type::<Collider>().add_systems(
         Update,
-        spawn.pipe(error).in_set(GameSystemSet::ColliderSpawn),
+        spawn
+            .after(TransformPropagate)
+            .run_if(in_state(GameState::Playing)),
     );
 }
 
+#[sysfail(Log<anyhow::Error, Error>)]
 fn spawn(
     collider_marker: Query<Entity, With<Collider>>,
-    mut commands: Commanmds,
+    mut commands: Commands,
     children: Query<&Children>,
     meshes: Res<Assets<Mesh>>,
-    mesh_handles: Query<&Handle<Mesh>>,
-) -> anyhow::Result<()> {
+    mesh_handles: Query<&Handle<Mesh>, Without<RigidBody>>,
+    global_transforms: Query<&GlobalTransform>,
+) {
     #[cfg(feature = "tracing")]
     let _span = info_span!("read_colliders").entered();
-    for parent in collider_marker.iter() {
-        for child in iter::once(parent).chain(children.iter_descendants(parent)) {
-            let Ok(mesh_handle) = mesh_handles.get(child) else {
-                continue;
-            };
-            // Unwrap cannot fail: we already load all the meshes at startup.
-            let mesh = meshes.get(mesh_handle).unwrap();
-            let collider = XpbdCollider::convex_hull_from_mesh(mesh)
-                .context("Failed to create collider from mesh")?;
-            commands.entity(child).insert((
-                collider,
-                CollisionLayers::new(
-                    [CollisionLayer::Terrain, CollisionLayer::CameraObstacle],
-                    [CollisionLayer::Character],
-                ),
-                NavMeshAffector,
-            ));
+    for entity in collider_marker.iter() {
+        let mut all_children_loaded = true;
+        for child in children.iter_descendants(entity) {
+            if let Ok(mesh_handle) = mesh_handles.get(child) {
+                if let Some(mesh) = meshes.get(mesh_handle) {
+                    let global_transforms = global_transforms
+                        .get(child)
+                        .context("Failed to get global transform while reading collider")?
+                        .compute_transform();
+                    let scaled_mesh = mesh.clone().scaled_by(global_transform.scale);
+                    let collider = XpbdCollider::trimesh_from_mesh(&scaled_mesh)
+                    .context("Failed to create collider from mesh")?;
+                commands.entity(child).insert((
+                    collider,
+                    RigidBody::Static,
+                    CollisionLayers::new(
+                        [CollisionLayer::Terrain,
+                        CollisionLayer::CameraObstacle],
+                        [CollisionLayer::Character],
+                    ),
+                    NavMeshAffector,
+                ));
+                } else {
+                    all_children_loaded = false;
+                }
+            }
         }
-        commands
-            .entity(parent)
-            .remove::<Collider>()
-            .insert(RigidBody::Static);
+        if all_children_loaded {
+            commands.entity(entity).remove::<Collider>();
+        }
     }
-    Ok(())
 }
