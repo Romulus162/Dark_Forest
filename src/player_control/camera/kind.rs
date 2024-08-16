6e6ae7352156cc96e32d7b249d66c392e9735f1f
Roman <Roman.badiere@gmail.com>
2024-08-16 03:34:04 +0200

    added bevy_dolly

--- src/player_control/camera/kind.rs
+++ src/player_control/camera/kind.rs
@@ -13,7 +13,7 @@ pub(super) fn update_kind(
     mut camera_query: Query<(&mut IngameCamera, &ActionState<CameraAction>)>,
     config: Res<GameConfig>,
 ) {
-    for (mut cameram, actions) in camera_query.iter_mut() {
+    for (mut camera, actions) in camera_query.iter_mut() {
         let zoom = actions.clamped_value(&CameraAction::Zoom);
         let zoomed_out = zoom < -1e-5;
         let zoomed_in = zoom > 1e-5;
@@ -45,7 +45,7 @@ pub(super) fn update_kind(
     }
 }
 
-pub(super) fn update_drivers(mut camera_query: Query<(IngameCamera, &mut Rig)>) {
+pub(super) fn update_drivers(mut camera_query: Query<(&IngameCamera, &mut Rig)>) {
     for (camera, mut rig) in camera_query.iter_mut() {
         match camera.kind {
             IngameCameraKind::ThirdPerson => set_third_person_drivers(&mut rig),
@@ -62,4 +62,46 @@ fn set_third_person_drivers(rig: &mut Rig) {
     rig.ensure_driver_exists(Arm::new(default()));
     // Overriding because tracking_predictive cannot be changed after creation.
     rig.override_driver(LookAt::new(default()).tracking_predictive(true));
+}
+
+fn set_first_person_drivers_without_target(rig: &mut Rig) {
+    rig.remove_driver::<LookAt>();
+    rig.remove_driver::<Arm>();
+}
+
+fn set_first_person_drivers_with_target(rig: &mut Rig) {
+    rig.remove_driver::<Arm>();
+    rig.override_driver(LookAt::new(default()));
+}
+
+fn set_fixed_angle_drivers(rig: &mut Rig) {
+    rig.ensure_driver_exists(Arm::new(default()));
+    rig.remove_driver::<LookAt>();
+}
+
+trait RigExt {
+    fn remove_driver<T: RigDriverTraits>(&mut self);
+    fn ensure_driver_exists<T: RigDriverTraits>(&mut self, driver: T);
+    fn override_driver<T: RigDriverTraits>(&mut self, driver: T) {
+        self.remove_driver::<T>();
+        self.ensure_driver_exists(driver);
+    }
+}
+
+impl RigExt for Rig {
+    fn remove_driver<T: RigDriverTraits>(&mut self) {
+        let index = self
+            .drivers
+            .iter()
+            .position(|driver| driver.as_ref().as_any().downcast_ref::<T>().is_some());
+        if let Some(index) = index {
+            self.drivers.remove(index);
+        }
+    }
+
+    fn ensure_driver_exists<T: RigDriverTraits>(&mut self, driver: T) {
+        if self.try_driver::<T>().is_none() {
+            self.drivers.push(Box::new(driver));
+        }
+    }
 }
