1998534f5184dfa837e5a5b8f41afe841e9ad22e
Roman <Roman.badiere@gmail.com>
2024-08-20 01:47:32 +0200

    stopping point

--- src/movement/physics.rs
+++ src/movement/physics.rs
@@ -3,7 +3,7 @@ use bevy_xpbd_3d::prelude::*;
 
 /// Sets up and configures the XPBD physics.
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins(PhysicsPlugin::default())
+    app.add_plugins(PhysicsPlugins::default())
     // Using the default fixed timestep causes issues on faster (165 Hz) machines.
     .insert_resource(Time::new_with(Physics::variable(1.0 / 60.)));
 }
