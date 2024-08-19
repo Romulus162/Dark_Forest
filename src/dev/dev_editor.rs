d946050812d2935f7b35781d9c8e301289a9f316
Roman <Roman.badiere@gmail.com>
2024-08-19 19:08:26 +0200

    oxidized_navigation going back to 0.10

--- src/dev/dev_editor.rs
+++ src/dev/dev_editor.rs
@@ -1,6 +1,4 @@
-// This below is commented out as it is calling for non-existent code
-use crate::
-{player_control::camera::ForceCursorGrabMode, GameState};
+use crate::{player_control::camera::ForceCursorGrabMode, GameState};
 use anyhow::Context;
 use bevy::{prelude::*, window::CursorGrabMode};
 use bevy_editor_pls::{
@@ -15,27 +13,27 @@ use serde::{Deserialize, Serialize};
 
 pub(super) fn plugin(app: &mut App) {
     app.init_resource::<DevEditorState>()
-    .add_editor_window::<DevEditorWindow>()
-    .add_systems(Update,
-        (handle_debug_render, set_cursor_grab_mode)
-            .run_if(in_state(GameState::Playing)),
-    );
+        .add_editor_window::<DevEditorWindow>()
+        .add_systems(
+            Update,
+            (handle_debug_render, set_cursor_grab_mode).run_if(in_state(GameState::Playing)),
+        );
 }
 
 pub(crate) struct DevEditorWindow;
 
 impl EditorWindow for DevEditorWindow {
     type State = DevEditorState;
-    const NAME: &'static str = "dark_forest Dev";
+    const NAME: &'static str = "Foxtrot Dev";
     const DEFAULT_SIZE: (f32, f32) = (200., 150.);
     fn ui(
         _world: &mut World,
-        mut cx:
-        bevy_editor_pls::editor_window::EditorWindowContext,
+        mut cx: bevy_editor_pls::editor_window::EditorWindowContext,
         ui: &mut egui::Ui,
     ) {
-        let state = cx.state_mut::<DevEditorState>()
-        .expect("Failed to get dev window state");
+        let state = cx
+            .state_mut::<DevEditorWindow>()
+            .expect("Failed to get dev window state");
 
         state.open = true;
         ui.heading("Debug Rendering");
@@ -78,7 +76,7 @@ fn set_cursor_grab_mode(
     for event in events.read() {
         if let EditorEvent::Toggle { now_active } = event {
             if *now_active {
-                force_cursor_grab.0 = Some(CurosrGrabMode::None);
+                force_cursor_grab.0 = Some(CursorGrabMode::None);
             } else {
                 force_cursor_grab.0 = None;
             }
